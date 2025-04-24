651f44f33766dfe462c18b9aa8dd695e66f198a7
LeeKM321 <stephen4951@gmail.com>
2025-04-24 09:49:19 +0900

    Header, Footer, Home 생성, Router 활성화(컴포넌트 형식으로)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <Header />
+      <div className='content-wrapper'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  // 라우터를 사용하려는 컴포넌트를 BrowserRouter로 감싸주세요.
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
