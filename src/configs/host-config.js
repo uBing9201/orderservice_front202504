5c8238581f4c0b3a01edc2e1868906133e544cc8
ubing <ubing0101@kakao.com>
2025-05-27 09:24:59 +0900

    host-config에 배포 서버의 host를 수정함.

--- src/configs/host-config.js
+++ src/configs/host-config.js
@@ -14,9 +14,12 @@ let backendHostName;
 if (clientHostName === 'localhost') {
   // 개발 중
   backendHostName = 'http://localhost:8000';
-} else if (clientHostName === 'playdatashop.com') {
+} else if (
+  clientHostName ===
+  'playdata-ooderservice9201.s3-website.ap-northeast-2.amazonaws.com'
+) {
   // 배포해서 현재 서비스 중
-  backendHostName = 'https://api.playdatashop.com';
+  backendHostName = 'http://3.36.86.10:8000';
 }
 
 export const API_BASE_URL = backendHostName;
