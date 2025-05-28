e88d7469af50f09e6a3f59583dfb9a6d05e90453
ubing <ubing0101@kakao.com>
2025-05-28 11:43:41 +0900

    새 주문이 들어오면 관리자에게 알림이 가는 서비스 초안 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,22 +6,28 @@ import {
   Toolbar,
   Typography,
 } from '@mui/material';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from '../context/UserContext';
 import { EventSourcePolyfill } from 'event-source-polyfill';
+import { API_BASE_URL, ORDER, SSE } from '../configs/host-config';
+import { NotificationAdd } from '@mui/icons-material';
 
 const Header = () => {
   // 로그인 상태에 따라 메뉴를 다르게 제공하고 싶다 -> Context에서 뽑아오면 되겠구나!
   const { isLoggedIn, onLogout, userRole } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  const [liveQuantity, setLiveQuantity] = useState(0); // 실시간 주문 수
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     console.log('role: ', userRole);
     const token = localStorage.getItem('ACCESS_TOKEN');
 
     if (userRole === 'ADMIN') {
-      // 알림을 ㅂ다기 우해 서버와 연결을 하기 위한 요청을 하겠다.
+      // 알림을 받기 위해 서버와 연결을 하기 위한 요청을 하겠다.
+      // 기존에 사용하던 fetch, axios는 지속적 연결을 지원하지 않는다.
       const sse = new EventSourcePolyfill(`${API_BASE_URL}${SSE}/subscribe`, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -32,9 +38,16 @@ const Header = () => {
         console.log(event);
       });
 
-      // 30초 마다 발생하는 알림.
+      // 30초마다 발생하는 알림. (연결을 유지하기 위해)
       sse.addEventListener('heartbeat', () => {
-        console.log('Received hearbeat');
+        console.log('Received heartbeat');
+      });
+
+      sse.addEventListener('ordered', (e) => {
+        const orderData = JSON.parse(e.data);
+        console.log(orderData);
+        setLiveQuantity((prev) => prev + 1);
+        setMessage(orderData.userEmail + '님의 주문!');
       });
     }
   }, [userRole]);
@@ -64,8 +77,8 @@ const Header = () => {
                   <Button color='inherit' component={Link} to='/product/manage'>
                     상품관리
                   </Button>
-                  <Button color='inherit' href='/order/list'>
-                    실시간 주문 ()
+                  <Button color='inherit' onClick={() => setLiveQuantity(0)}>
+                    실시간 주문 <NotificationAdd /> ({liveQuantity}) {message}
                   </Button>
                 </>
               )}
